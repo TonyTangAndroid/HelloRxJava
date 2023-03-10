3028f1c1189838bc7b54b466d24a0ed1553f5cc7
TonyTang <tonytang@uber.com>
2023-03-10 15:27:00 -0800

    Add a bunch of demo modules

---
+++ demo_fragment/src/main/java/com/hello/demo_fragment/MainActivity.java
@@ -0,0 +1,19 @@
+package com.hello.demo_fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.hello.demo_fragment.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    if (savedInstanceState == null) {
+      getSupportFragmentManager().beginTransaction()
+          .replace(R.id.container, MainFragment.newInstance())
+          .commitNow();
+    }
+  }
+}
