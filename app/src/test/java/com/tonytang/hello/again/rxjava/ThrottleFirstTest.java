cfe98aa90a53b9a6944e83a3a9033cd0553c1a47
TonyTang <tonytang@uber.com>
2023-03-10 14:29:44 -0800

    Add demo code to assert that the throttleFirst does not switch thread.

---
+++ app/src/test/java/com/tonytang/hello/again/rxjava/ThrottleFirstTest.java
@@ -0,0 +1,21 @@
+package com.tonytang.hello.again.rxjava;
+
+import com.google.common.truth.Truth;
+import io.reactivex.Observable;
+import io.reactivex.observers.TestObserver;
+import io.reactivex.schedulers.Schedulers;
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+
+public class ThrottleFirstTest {
+
+  @Test
+  public void without_cache_would_execute_footprint_three_times() {
+
+    TestObserver<Integer> test = Observable.just(1).throttleLatest(1, TimeUnit.SECONDS, Schedulers.computation()).test();
+    Thread thread = test.lastThread();
+    Truth.assertThat(thread.getName()).isEqualTo("Test worker");
+
+  }
+
+}
